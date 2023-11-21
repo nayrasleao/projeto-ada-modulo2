ca7cebcf5f1e21d8077b3f0b2ac2b043a416cc0a
portellaluana <luanaportella@gmail.com>
2023-11-21 20:11:02 -0300

    wip/ press enter

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ function enviaTarefa() {
   const input = document.getElementById("input")
   const lista = document.getElementById("lista")
 
+  //criar tarefa com enter
   if (input.value !== "" && input.value !== null && input.value !== undefined) {
     const novaTarefa = document.createElement("li")
     novaTarefa.innerText = input.value
