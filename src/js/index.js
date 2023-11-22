92b80af562783b49fd695f320aed64c5f9654083
N3th3rL0rd <hackerhel@gmail.com>
2023-11-22 16:01:37 -0300

    Refactor code for adding and displaying tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,60 @@
-const input = document.getElementById("input")
-const lista = document.getElementById("lista")
+const input = document.getElementById("input");
+const lista = document.getElementById("lista");
 
+//aqui salva a lista no socalStorage
+function salvarLista() {
+  const tarefas = [];
+  lista.querySelectorAll("li").forEach((tarefa) => {
+    tarefas.push(tarefa.innerText);
+  });
+  localStorage.setItem("listaTarefas", JSON.stringify(tarefas));
+}
+
+// aqui carrega a lista do LocalStorage na pagina, se tiver
+window.onload = function () {
+  if (localStorage.getItem("listaTarefas")) {
+    const tarefasSalvas = JSON.parse(localStorage.getItem("listaTarefas"));
+    tarefasSalvas.forEach((tarefa) => {
+      const li = document.createElement("li");
+      li.innerText = tarefa;
+
+      const btn = document.createElement("button");
+      btn.innerText = "x";
+      btn.onclick = function () {
+        lista.removeChild(li);
+        salvarLista();
+      };
+
+      li.appendChild(btn);
+      lista.appendChild(li);
+      input.value = "";
+    });
+  }
+};
 
 function enviaTarefa() {
   if (input.value !== "" && input.value !== null && input.value !== undefined) {
-    const li = document.createElement("li")
-    li.innerText = input.value
+    const li = document.createElement("li");
+    li.innerText = input.value;
 
-    const btn = document.createElement("button")
-    btn.innerText = "x"
+    const btn = document.createElement("button");
+    btn.innerText = "x";
     btn.onclick = function () {
-      lista.removeChild(li)
-    }
-    
-    li.appendChild(btn)
-    lista.appendChild(li)
-    input.value = ""
-  } else alert('Digite uma tarefa válida')
+      lista.removeChild(li);
+      salvarLista();
+    };
 
+    li.appendChild(btn);
+    lista.appendChild(li);
+    input.value = "";
 
-    // Adicionar um evento de clique para riscar/desriscar a tarefa
-    novaTarefa.addEventListener("click", function () {
-    novaTarefa.classList.toggle("concluido")
-    })
+    salvarLista();
+  } else {
+    alert("Digite uma tarefa válida");
   }
 }
-
-input.addEventListener('keypress', function(e) {
-  if (e.key === 'Enter') {
-    enviaTarefa()
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    enviaTarefa();
   }
 });
